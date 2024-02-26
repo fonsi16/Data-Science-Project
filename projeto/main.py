f1207aea0ed3b7256bb83197d98a3f75d716113c
afons <mikerodri1602@gmail.com>
2024-02-26 15:33:04 +0000

    Data class modifications and new features, bmi and sleep class

--- projeto/main.py
+++ projeto/main.py
@@ -29,6 +29,37 @@ class DataAnalysis:
         print("\nStatistical distribution of each variable:")
         print(self.df.describe())
 
+    """
+        1 -> severe thinness
+        2 -> moderate thinness
+        3 -> mild thinness
+        4 -> normal
+        5 -> overweight
+        6 -> obese class 1
+        7 -> obese class 2
+        8 -> obese class 3
+    """
+
+    def bmi_feature(self):
+        bmi = self.df["BMI"]
+        condition = [bmi < 16, bmi < 17, bmi < 18.5, bmi < 25, bmi < 30, bmi < 35, bmi < 40, bmi >= 40]
+        choice = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.df["BmiClass"] = np.select(condition, choice)
+
+    def sleep_feature(self):
+        sleep = self.df["SleepTime"]
+        condition = [sleep < 6, sleep < 9, sleep >= 9]
+        choice = [1, 2, 3]
+        self.df["SleepClass"] = np.select(condition, choice)
+
+    def count_plots(self):
+        for i in range(len(self.df.columns)):
+            column = self.df.columns[i]
+            if column == 'BMI' or column == 'HeartDisease':
+                continue
+            else:
+                sns.countplot(x=column, data=self.df, hue=self.target)
+                plt.show()
 
 
 path = 'data/heart_2020.csv'
@@ -39,6 +70,10 @@ data_analysis_instance = DataAnalysis(path, 'HeartDisease')
 # Verify the presence of duplicated data and remove it
 data_analysis_instance.verify_and_delete_duplicates()
 
+# Create features
+data_analysis_instance.bmi_feature()
+data_analysis_instance.sleep_feature()
+
 data_analysis_instance.describe_variables()
 
-
+data_analysis_instance.count_plots()
